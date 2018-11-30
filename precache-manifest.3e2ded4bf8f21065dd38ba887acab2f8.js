88a15bbef869550da09f1d95d71f1e5f84570b8c
karlos <sky_karlos@hotmail.com>
2018-11-30 09:22:54 -0500

    Updates

---
+++ precache-manifest.3e2ded4bf8f21065dd38ba887acab2f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de31f08e1a7a7a69d023",
+    "url": "/final-web-avanzada-game/static/js/runtime~main.de31f08e.js"
+  },
+  {
+    "revision": "3952f1cd0f93bac28576",
+    "url": "/final-web-avanzada-game/static/js/main.3952f1cd.chunk.js"
+  },
+  {
+    "revision": "f88d66800aa66b6f0e80",
+    "url": "/final-web-avanzada-game/static/js/1.f88d6680.chunk.js"
+  },
+  {
+    "revision": "3952f1cd0f93bac28576",
+    "url": "/final-web-avanzada-game/static/css/main.1ea62c24.chunk.css"
+  },
+  {
+    "revision": "206df94f85115f51ccfb7660419b61f8",
+    "url": "/final-web-avanzada-game/index.html"
+  }
+];
