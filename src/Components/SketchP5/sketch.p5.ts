5c4fc53ecc55a8fa084962ed40ddde7a59a2685b
karlos <sky_karlos@hotmail.com>
2018-12-01 18:20:48 -0500

    testing async

--- src/Components/SketchP5/sketch.p5.ts
+++ src/Components/SketchP5/sketch.p5.ts
@@ -17,101 +17,110 @@ export const sketch = (width: number, height: number, props: SketchProps) => {
         let redInvulnerability: boolean = props.redInvulnerability;
         let playerSpaceShip: CharacterSpaceship | null;
         let enemySpaceShip: EnemyShip | null;
+        let mounted: boolean = true;
         p.setup = () => {
-            // p.frameRate(60);
             p.ellipseMode(p.CENTER);
             p.textAlign(p.CENTER);
             // p.noSmooth();
             playerSpaceShip = new CharacterSpaceship(p, width, height);
             enemySpaceShip = new EnemyShip(p, width, height);
+            fixedUpdate();
         };
 
         p.draw = () => {
             p.background(33,33,33, Math.floor(p.map(p.sin(p.radians(p.frameCount)), -1, 1, 40,  70)));
             // p.clear();
             if (playerSpaceShip) {
-                playerSpaceShip.moveSpaceship();
                 playerSpaceShip.update();
                 playerSpaceShip.display();
             }
 
             if(enemySpaceShip) {
-                enemySpaceShip.update();
                 enemySpaceShip.display();
             }
-
-            checkBulletsCollision();
-            checkExplosionCollision();
         };
-        
-        function checkBulletsCollision() {
-            if (playerSpaceShip) {
-                playerSpaceShip.shots.forEach((bullet: Bullet) => {
-                    if (enemySpaceShip && Vector.dist(bullet.pos, enemySpaceShip.pos) < (enemySpaceShip.diameter * 0.95) / 2 && playerSpaceShip) {
-                        playerSpaceShip.shots.splice(playerSpaceShip.shots.indexOf(bullet), 1);
-                        enemySpaceShip.live--;
-                        if (enemySpaceShip.live === 0) {
-                            enemySpaceShip.destroyEnemy().then(() => {
-                                console.log('mulio');
-                                enemySpaceShip = null;
-                            });
-                        }
-                    }
-                })
+
+        async function fixedUpdate() {
+            while (mounted) {
+                await checkExplosionCollision();
+                await checkBulletsCollision();
             }
         }
 
+        function checkBulletsCollision(): Promise<void> {
+            return new Promise<void>(resolve => {
+                if (playerSpaceShip) {
+                    playerSpaceShip.shots.forEach((bullet: Bullet) => {
+                        if (enemySpaceShip && Vector.dist(bullet.pos, enemySpaceShip.pos) < (enemySpaceShip.diameter * 0.95) / 2 && playerSpaceShip) {
+                            playerSpaceShip.shots.splice(playerSpaceShip.shots.indexOf(bullet), 1);
+                            enemySpaceShip.live--;
+                            if (enemySpaceShip.live === 0) {
+                                enemySpaceShip.destroyEnemy().then(() => {
+                                    enemySpaceShip = null;
+                                });
+                            }
+                        }
+                    })
+                }
+                setTimeout(() => {
+                    resolve();
+                }, 13.3)
+            });
+        }
 
-        function checkExplosionCollision() {
+        function checkExplosionCollision(): Promise<void> {
+            return new Promise<void>(resolve => {
+                if (enemySpaceShip) {
 
-            if (enemySpaceShip) {
+                    enemySpaceShip.explosionWaves.forEach((wave: ExplosionWave) => {
 
-                enemySpaceShip.explosionWaves.forEach((wave: ExplosionWave) => {
+                        if (playerSpaceShip) {
 
-                    if (playerSpaceShip) {
+                            const diameterFixed = ((wave.diameter * 0.97) / 2);
+                            const superiorDiameter = diameterFixed + (wave.strokeSize / 2);
+                            const inferiorDiameter = diameterFixed - (wave.strokeSize / 2);
 
-                        const diameterFixed = ((wave.diameter * 0.97) / 2);
-                        const superiorDiameter = diameterFixed + (wave.strokeSize / 2);
-                        const inferiorDiameter = diameterFixed - (wave.strokeSize / 2);
+                            // SUPERIOR EXPLOSION CIRCUMFERENCE
+                            const superiorVector = Vector.sub(playerSpaceShip.pos, wave.pos);
+                            superiorVector.normalize();
+                            superiorVector.mult(superiorDiameter);
 
-                        // SUPERIOR EXPLOSION CIRCUMFERENCE
-                        const superiorVector = Vector.sub(playerSpaceShip.pos, wave.pos);
-                        superiorVector.normalize();
-                        superiorVector.mult(superiorDiameter);
-
-                        const superiorTarget: Vector = wave.pos.copy();
-                        superiorTarget.add(superiorVector);
-                        // p.stroke(0, 200, 0);
-                        // p.line(wave.pos.x, wave.pos.y, superiorTarget.x, superiorTarget.y);
+                            const superiorTarget: Vector = wave.pos.copy();
+                            superiorTarget.add(superiorVector);
+                            // p.stroke(0, 200, 0);
+                            // p.line(wave.pos.x, wave.pos.y, superiorTarget.x, superiorTarget.y);
 
 
-                        // INFERIOR EXPLOSION CIRCUMFERENCE
-                        const inferiorVector = Vector.sub(playerSpaceShip.pos, wave.pos);
-                        inferiorVector.normalize();
-                        inferiorVector.mult(inferiorDiameter);
+                            // INFERIOR EXPLOSION CIRCUMFERENCE
+                            const inferiorVector = Vector.sub(playerSpaceShip.pos, wave.pos);
+                            inferiorVector.normalize();
+                            inferiorVector.mult(inferiorDiameter);
 
-                        const inferiorTarget: Vector = wave.pos.copy();
-                        inferiorTarget.add(inferiorVector);
-                        // p.stroke(0, 0, 200);
-                        // p.line(wave.pos.x, wave.pos.y, inferiorTarget.x, inferiorTarget.y);
+                            const inferiorTarget: Vector = wave.pos.copy();
+                            inferiorTarget.add(inferiorVector);
+                            // p.stroke(0, 0, 200);
+                            // p.line(wave.pos.x, wave.pos.y, inferiorTarget.x, inferiorTarget.y);
 
-                        if ((Vector.dist(wave.pos, playerSpaceShip.pos) > Vector.dist(wave.pos, inferiorTarget)) && (Vector.dist(wave.pos, playerSpaceShip.pos) < Vector.dist(wave.pos, superiorTarget))) {
-                            //  console.log('dañooooo: ' + wave.strokeSize);
-                            if (!playerSpaceShip.invulnerable && !playerSpaceShip.redInvulnerable) {
-                                playerSpaceShip.damaged();
-                                if (playerSpaceShip.live === 0) {
-                                    console.log('mulioPLayel');
-                                    playerSpaceShip = null;
+                            if ((Vector.dist(wave.pos, playerSpaceShip.pos) > Vector.dist(wave.pos, inferiorTarget)) && (Vector.dist(wave.pos, playerSpaceShip.pos) < Vector.dist(wave.pos, superiorTarget))) {
+                                //  console.log('dañooooo: ' + wave.strokeSize);
+                                if (!playerSpaceShip.invulnerable && !playerSpaceShip.redInvulnerable) {
+                                    playerSpaceShip.damaged();
+                                    if (playerSpaceShip.live === 0) {
+                                        console.log('mulioPLayel');
+                                        playerSpaceShip = null;
+                                    }
                                 }
                             }
                         }
-                    }
 
-                });
+                    });
 
 
-            }
-
+                }
+                setTimeout(() => {
+                    resolve();
+                }, 13.3)
+            });
         }
 
         p.receiveProps = (nextProps: SketchProps) => {
@@ -159,6 +168,7 @@ export const sketch = (width: number, height: number, props: SketchProps) => {
             if (playerSpaceShip) {
                 playerSpaceShip.unmount();
             }
+            mounted = false;
         }
     }
 };
@@ -219,6 +229,8 @@ class CharacterSpaceship {
                 this.shots.splice( this.shots.indexOf(bullet), 1 );
             }
         });
+
+        this.moveSpaceship();
     }
 
     display() {
@@ -387,6 +399,7 @@ class EnemyShip {
     explosionWaves: ExplosionWave[];
     private alive: boolean;
     private moving: boolean;
+    private brake: boolean;
     private resolveMovePromise: (() => void) | undefined;
     private resolveDeath: (() => void) | undefined;
     private readonly objective: Vector;
@@ -407,10 +420,13 @@ class EnemyShip {
         this.explosionWaves = [];
         this.alive = true;
         this.moving = true;
+        this.brake = false;
 
         this.objective = this.p5Instance.createVector(this.p5Instance.random(0, this.sketchWidth), this.p5Instance.random(0, this.sketchHeight));
 
         this.shotWave();
+
+        this.updatePhysics();
 
     }
 
@@ -426,7 +442,7 @@ class EnemyShip {
     }
 
     move(): Promise<void> {
-        this.objective.set(this.p5Instance.random(this.sketchWidth * 0.1, this.sketchWidth * 0.8), this.p5Instance.random(this.sketchHeight * 0.1, this.sketchHeight * 0.6));
+        this.objective.set(this.p5Instance.random(this.sketchWidth * 0.15, this.sketchWidth * 0.75), this.p5Instance.random(this.sketchHeight * 0.15, this.sketchHeight * 0.55));
         this.moving = true;
         return new Promise<void>(resolve => {
             this.resolveMovePromise = resolve;
@@ -438,27 +454,68 @@ class EnemyShip {
         this.acceleration.add(f);
     }
 
-    update() {
+    async updatePhysics() {
+        while (this.live) {
+            await this.update();
+        }
+    }
 
-        if (this.moving) {
-            if (Vector.dist(this.objective, this.pos) < this.diameter * 0.6) {
-                this.friction = 0.8 * this.vel.mag();
-                if (this.vel.mag() < 0.8) {
-                    this.vel.mult(0);
-                    this.acceleration.mult(0);
-                    this.friction = this.mass * 0.01;
-                    this.moving = false;
-                    if (this.resolveMovePromise) {
-                        this.resolveMovePromise();
+    update(): Promise<void> {
+        return new Promise<void>(resolve => {
+            if (this.moving) {
+                if (this.brake) {
+                    if (this.vel.mag() < 0.2) {
+                        this.vel.mult(0);
+                        this.acceleration.mult(0);
+                        this.friction = this.mass * 0.01;
+                        this.moving = false;
+                        this.brake = false;
+                        if (this.resolveMovePromise) {
+                            this.resolveMovePromise();
+                        }
+                    }
+                } else {
+                    const dir = Vector.sub(this.objective, this.pos);
+                    dir.normalize();
+                    dir.mult(2);
+                    this.applyForce(dir);
+
+                    if (Vector.dist(this.objective, this.pos) < this.diameter * 0.2) {
+                        this.brake = true;
+                        this.friction = this.vel.mag() * 0.3;
                     }
                 }
-            } else {
-                const dir = Vector.sub(this.objective, this.pos);
-                dir.normalize();
-                dir.mult(2);
-                this.applyForce(dir);
+
             }
-        }
+
+
+
+            const frictionVector: Vector = this.vel.copy();
+            frictionVector.mult(-1);
+            frictionVector.normalize();
+            frictionVector.mult(this.friction);
+            this.applyForce(frictionVector);
+
+            this.vel.add(this.acceleration);
+            this.pos.add(this.vel);
+            this.acceleration.mult(0);
+
+
+            this.explosionWaves.forEach((explosion: ExplosionWave) => {
+                if (explosion.diameter > 10000) {
+                    explosion.alive = false;
+                    this.explosionWaves.splice(this.explosionWaves.indexOf(explosion), 1);
+                }
+            });
+
+            this.vel.limit(20);
+            setTimeout(() => {
+                resolve();
+            }, 13.3);
+        });
+    }
+
+    display() {
 
         if (!this.alive) {
             this.fillOpacity--;
@@ -467,30 +524,10 @@ class EnemyShip {
             }
         }
 
-
-
-        const frictionVector: Vector = this.vel.copy();
-        frictionVector.mult(-1);
-        frictionVector.normalize();
-        frictionVector.mult(this.friction);
-        this.applyForce(frictionVector);
-
-        this.vel.add(this.acceleration);
-        this.pos.add(this.vel);
-        this.acceleration.mult(0);
-
-
         this.explosionWaves.forEach((explosion: ExplosionWave) => {
             explosion.display();
-            if (explosion.diameter > 10000) {
-                this.explosionWaves.splice(this.explosionWaves.indexOf(explosion), 1);
-            }
         });
 
-        this.vel.limit(20);
-    }
-
-    display() {
         this.p5Instance.stroke(244,67,54, this.fillOpacity);
         this.p5Instance.strokeWeight(this.p5Instance.map(this.p5Instance.cos(this.p5Instance.radians(this.p5Instance.frameCount * 3)), -1, 1, this.diameter * 0.03,  this.diameter * 0.09));
         this.p5Instance.fill(230, 100, 100,this.fillOpacity * 0.7);
@@ -500,11 +537,11 @@ class EnemyShip {
     async releaseExplosion(): Promise<void> {
             for (let i = 0; i < this.p5Instance.random(2, 6); i++) {
                 if (this.alive) {
-                    this.explosionWaves.push(new ExplosionWave(this.pos.copy(), this.diameter, this.p5Instance.random(1, 4.5), this.p5Instance));
+                    this.explosionWaves.push(new ExplosionWave(this.pos.copy(), this.diameter, this.p5Instance.random(1, 1), this.p5Instance));
                     if (i < 5) {
                         await this.delayBetweenExplosions(this.p5Instance.random(500, 2000));
                     } else {
-                        await this.delayBetweenExplosions(20);
+                        await this.delayBetweenExplosions(100);
                     }
                 }
             }
@@ -536,6 +573,7 @@ class ExplosionWave {
     diameter: number;
     strokeMultiplier: number;
     strokeSize: number;
+    alive: boolean;
 
     constructor(_pos: Vector, _initialDiameter: number, _strokeMultiplier: number, _pInstance: p5) {
         this.pInstance = _pInstance;
@@ -543,6 +581,8 @@ class ExplosionWave {
         this.diameter = _initialDiameter;
         this.strokeMultiplier = _strokeMultiplier;
         this.strokeSize = (this.diameter * 0.05) * this.strokeMultiplier;
+        this.alive = true;
+        this.updateExpansion();
     }
 
     display() {
@@ -551,11 +591,21 @@ class ExplosionWave {
         this.pInstance.strokeWeight(this.strokeSize);
         this.pInstance.noFill();
         this.pInstance.ellipse(this.pos.x, this.pos.y, this.diameter, this.diameter);
-        this.update();
     }
 
-    update() {
-        this.diameter += (this.diameter * 0.1) / this.strokeMultiplier;
+    async updateExpansion() {
+        while (this.alive) {
+            await this.explosionExpansion();
+        }
+    }
+
+    explosionExpansion(): Promise<void> {
+        return new Promise<void>(resolve => {
+            this.diameter += (this.diameter * 0.1) / this.strokeMultiplier;
+            setTimeout(() => {
+                resolve();
+            }, 13.3)
+        });
     }
 }
 
